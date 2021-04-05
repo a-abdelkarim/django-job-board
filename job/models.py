453e78a1828775906981d165275c48fbb1ea662b
a-abdelkarim <a.abdelkarim9696@gmail.com>
2021-04-05 15:51:20 +0200

    add job models

--- job/models.py
+++ job/models.py
@@ -9,6 +9,7 @@ class Job(models.Model):
     
     title = models.CharField(max_length=100)
     job_type = models.CharField(max_length=15, choices=JOB_TYPE)
+    #category = models.ForeignKey('Category', on_delete=models.CASCADE)
     descrption = models.TextField(max_length=1000, null=True)
     published_at = models.DateTimeField(auto_now=True)
     vacancy = models.IntegerField(default=1)
@@ -17,3 +18,10 @@ class Job(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
