132278bf3fd91aac8245d77aca7f7eb066d33670
a-abdelkarim <a.abdelkarim9696@gmail.com>
2021-04-06 13:04:28 +0200

    #

--- job/models.py
+++ job/models.py
@@ -5,12 +5,16 @@ JOB_TYPE = (
     ('Part Time', 'Part Time'),
 )
 
+def image_upload(instance, filename):
+    imagname, extention = filename.split('.')
+    return "jobs/%s_%s_%s.%s"%(instance.id, instance.title, instance.published_at, extention)
+
 class Job(models.Model):
     
     title = models.CharField(max_length=100)
     job_type = models.CharField(max_length=15, choices=JOB_TYPE)
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
-    img = models.ImageField(upload_to='jobs/')
+    img = models.ImageField(upload_to=image_upload, default='no-image.png')
     descrption = models.TextField(max_length=1000, null=True)
     published_at = models.DateTimeField(auto_now=True)
     vacancy = models.IntegerField(default=1)
